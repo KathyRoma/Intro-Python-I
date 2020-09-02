031d542ba839a2033ac63af18e64a06d40f9573b
Ekaterina Romanovskaya <64712690+KathyRoma@users.noreply.github.com>
2020-09-01 21:05:56 -0700

    p1 day2

--- src/07_slices.py
+++ src/07_slices.py
@@ -12,7 +12,7 @@ Use Python's slice syntax to achieve the following:
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+print(a[1])
 
 # Output the second-to-last element: 9
 print()
@@ -20,18 +20,40 @@ print()
 # Output the last three elements in the array: [7, 9, 6]
 print()
 
+for i in a[-3:]:
+    print(i)
+
 # Output the two middle elements in the array: [1, 7]
 print()
 
+def print_middle(lst):
+    if len(lst)%2 == 0:
+        print(lst[int(len(lst)/2) -1], lst[int(len(lst)/2)])
+    else:
+        print(lst[int(len(lst)/2)])
+
+
+def print_middle(lst):
+    m = int(len(lst)/2)
+    if len(lst)%2 == 0:
+        print(lst[m -1], lst[m])
+    else:
+        print(lst[m])
 # Output every element except the first one: [4, 1, 7, 9, 6]
 print()
 
+print(a[1:])
+
 # Output every element except the last one: [2, 4, 1, 7, 9]
 print()
-
+print(a[:-1])
 # For string s...
 
 s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
-print()
+l=len('world')
+print(s[(-2 -l):-1])
+
+ss = s.split(sep=' ')
+print(ss[-1][:-1])
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -12,12 +12,16 @@ for more info regarding list comprehensions.
 
 y = []
 
+y = [x for x in range(1, 6)]
+
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+
+y = [x**3 for x in range(0, 10)]
 
 print(y)
 
@@ -28,6 +32,9 @@ a = ["foo", "bar", "baz"]
 
 y = []
 
+# y = [a[i].upper() for i in range(0, (len(a)-1)]
+y = [i.upper() for i in a]
+
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
@@ -37,5 +44,6 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 y = []
+y = [int(i) for i in x if int(i)%2==0]
 
 print(y)
